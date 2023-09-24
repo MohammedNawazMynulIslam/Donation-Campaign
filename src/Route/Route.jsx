4225224ac983ce29fbee63a0153bfbe3dda0b325
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-09-24 21:08:44 +0600

    added essential folder

---
+++ src/Route/Route.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const Route = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+export default Route;
