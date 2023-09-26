ca39fd73ce144c86752e75de4c2249eb189284a2
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-09-26 07:22:41 +0600

    added see all button

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -4,6 +4,7 @@ import DonationSectionCard from "../Components/DonationsCards/DonationSection/Do
 const Donation = () => {
   const [donation, setDonation] = useState([]);
   const [noDataFound, setNoDataFound] = useState("");
+  const [ShowMore, setShowMore] = useState(false);
 
   useEffect(() => {
     const donatedCard = JSON.parse(localStorage.getItem("donation"));
@@ -22,15 +23,32 @@ const Donation = () => {
       ) : (
         <div>
           <div className=" grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4">
-            {donation.map((card) => (
-              <DonationSectionCard
-                key={card.id}
-                card={card}
-              ></DonationSectionCard>
-            ))}
+            {ShowMore
+              ? donation.map((card) => (
+                  <DonationSectionCard
+                    key={card.id}
+                    card={card}
+                  ></DonationSectionCard>
+                ))
+              : donation
+                  .slice(0, 4)
+                  .map((card) => (
+                    <DonationSectionCard
+                      key={card.id}
+                      card={card}
+                    ></DonationSectionCard>
+                  ))}
           </div>
         </div>
       )}
+      <div className="text-center mt-5">
+        <button
+          onClick={() => setShowMore(!ShowMore)}
+          className="btn btn-accent text-white"
+        >
+          See All
+        </button>
+      </div>
     </div>
   );
 };
